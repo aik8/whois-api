4d6d2dc34946e87b2a22fb748ead09a424316435
John Kitsantas <proteus@devoid.gr>
2018-04-23 15:53:16 +0300

    feat: create a route to perform the whois requests

---
+++ routes/whois.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const jsdom = require('jsdom');
+const JSDOM = jsdom.JSDOM;
+const _ = require('lodash');
+
+router.get('/', (req, res, next) => {
+	axios.get(`http://grwhois.ics.forth.gr:800/plainwhois/plainWhois?domainName=${req.query.domain}`)
+		.then(response => response.data) // Extract the data from the response.
+		.then(response => new JSDOM(response)) // Parse the HTML in the response.
+		.then(dom => {
+			// Get the contents of the body, removing all new line chars.
+			let result = _.replace(dom.window.document.body.innerHTML, /\n/gm, '');
+			let response = {};
+
+			// If the query returned a result, it will contain <br> elements.
+			if (/.*?(?=<br>)/gm.test(result)) {
+				// Remove all new line chars and <br> elements, replaced by commas.
+				let filtered = _.split(_.replace(result, /<br>/gm, ','), ',');
+				// The last element of the resulting array is always an empty string.
+				filtered.pop();
+
+				// Start forming the response, which will be a JSON object.
+				let fields = _.map(filtered, row => {
+					let field = row.split(':');
+					return { key: field[0], value: field[1] };
+				});
+				response.fields = fields;
+
+				// Add the "result" property, to fill in the appropriate GUI element.
+				response.result = 'Domain already registered!';
+				response.success = true;
+			} else {
+				// In case the request was unsuccessful the result will only
+				// contain the error message. Thus the response's "result"
+				// property will is the best to accommodate it.
+				response = { result, success: false };
+			}
+			
+			res.render('index', { title: `Αποτελέσματα για το ${req.query.domain}`, response });
+		})
+		.catch(next);
+});
+
+module.exports = router;
