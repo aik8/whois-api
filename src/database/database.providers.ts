0a5e5045dc647574821d5a8549401e73ff23d260
John Kitsantas <proteus@devoid.gr>
2019-04-30 00:21:12 +0300

    feat: create the database module with a sequelize provider

---
+++ src/database/database.providers.ts
@@ -0,0 +1,40 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Domain } from '../models/domains/domain.entity';
+import { Snapshot } from 'src/models/snapshots/snapshot.entity';
+import { Registrar } from 'src/models/registrars/registrar.entity';
+import { NameServer } from 'src/models/ns/ns.entity';
+import { SnapshotNameservers } from 'src/models/snapshots/snapshot-ns.entity';
+
+export const databaseProviders = [
+	{
+		provide: 'SEQUELIZE',
+		useFactory: async () => {
+			const sequelize = new Sequelize({
+				operatorsAliases: false,
+				dialect: 'mariadb',
+				host: 'localhost',
+				port: 3306,
+				username: 'whois',
+				password: 'siohw',
+				database: 'whois'
+			});
+
+			sequelize.addModels([
+				Domain,
+				NameServer,
+				Registrar,
+				Snapshot,
+				SnapshotNameservers
+			]);
+
+			Domain.hasMany(Snapshot, { as: 'snapshots' });
+			Snapshot.hasOne(Registrar, { as: 'registrar' });
+
+			Snapshot.belongsToMany(NameServer, { through: { model: SnapshotNameservers }});
+			NameServer.belongsToMany(Snapshot, { through: SnapshotNameservers });
+
+			await sequelize.sync();
+			return sequelize;
+		}
+	}
+];
