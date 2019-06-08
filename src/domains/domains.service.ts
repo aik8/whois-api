3a29ce44770337ecabab8c2545fcee1c1b8fb57d
John Kitsantas <proteus@devoid.gr>
2019-06-08 11:14:05 +0300

    feat: add some joins to domains and registrars

--- src/domains/domains.service.ts
+++ src/domains/domains.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { RestfulOptions } from '@nestjsx/crud';
 import { RepositoryService } from '@nestjsx/crud/typeorm';
+import { Domain } from '../models';
 import { Repository } from 'typeorm';
-import { Domain } from 'src/models/domain.entity';
 
 @Injectable()
 export class DomainsService extends RepositoryService<Domain> {
+	protected options: RestfulOptions = {
+		join: {
+			snapshots: {}
+		}
+	};
+
 	constructor(@InjectRepository(Domain) repo: Repository<Domain>) {
 		super(repo);
 	}
--- src/registrars/registrars.service.ts
+++ src/registrars/registrars.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RepositoryService } from '@nestjsx/crud/typeorm';
 import { Repository } from 'typeorm';
-import { Registrar } from 'src/models/registrar.entity';
+import { Registrar } from '../models';
 
 @Injectable()
 export class RegistrarsService extends RepositoryService<Registrar> {
-	constructor(@InjectRepository(Registrar) repo: Repository<Registrar>) {
-		super(repo);
+	constructor(@InjectRepository(Registrar) registrars: Repository<Registrar>) {
+		super(registrars);
 	}
 }
