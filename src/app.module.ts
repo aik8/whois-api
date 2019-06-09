4b05148ac524ba3d30188128d956d7588e3ff8d6
John Kitsantas <proteus@devoid.gr>
2019-06-09 21:35:49 +0300

    refactor(app): use config only from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DomainsModule } from './domains/domains.module';
+import { Domain, NameServer, Registrar, Snapshot } from './models';
 import { NameServersModule } from './name-servers/name-servers.module';
 import { PiosModule } from './pios/pios.module';
 import { RegistrarsModule } from './registrars/registrars.module';
@@ -10,7 +11,16 @@ import { SnapshotsModule } from './snapshots/snapshots.module';
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: process.env.TYPEORM_CONNECTION as any,
+			host: process.env.TYPEORM_HOST,
+			username: process.env.TYPEORM_USERNAME,
+			password: process.env.TYPEORM_PASSWORD,
+			database: process.env.TYPEORM_DATABASE,
+			port: Number.parseInt(process.env.TYPEORM_PORT, 10),
+			entities: [Domain, NameServer, Registrar, Snapshot],
+			migrations: ['../migrations/{.js,.ts}']
+		}),
 		NameServersModule,
 		RegistrarsModule,
 		DomainsModule,
