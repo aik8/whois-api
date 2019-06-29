555570642ec14c39578b2869adabcd613e734e3f
John Kitsantas <proteus@devoid.gr>
2019-06-29 16:40:21 +0300

    fix(pios): the "fast" parameter was not being parsed correctly

--- src/pios/pios.controller.ts
+++ src/pios/pios.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query, HttpStatus, HttpCode } from '@nestjs/common';
 import { SnapshotsService } from '../snapshots/snapshots.service';
 import { PiosService } from './pios.service';
+import { ParseBooleanPipe } from './parse-boolean.pipe';
 
 @Controller('pios')
 export class PiosController {
@@ -10,14 +11,12 @@ export class PiosController {
 	) { }
 
 	@Get()
-	@HttpCode(200)
-	whois(@Query('domain') domain: string, @Query('fast') fast: boolean = false) {
-		if (fast) { return Promise.resolve(); }
-		return this.piosQuery(domain);
-	}
-
-	piosQuery(domain: string) {
-		return this.pios.query(domain)
+	whois(@Query('domain') domain: string, @Query('fast', new ParseBooleanPipe()) fast: boolean = false) {
+		// Execute the query and keep the resulting Promise.
+		const result = this.pios.query(domain)
 			.then(this.snapshots.createSnapshot);
+
+		const response = fast as boolean === true ? Promise.resolve() : result;
+		return response;
 	}
 }
