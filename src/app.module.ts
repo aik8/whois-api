2310da0d04c18e6da74ff35076052d4664058ddf
John Kitsantas <proteus@devoid.gr>
2019-12-01 20:45:37 +0200

    refactor(app): update the default TypeORM config values

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,16 +12,16 @@ import { SnapshotsModule } from './snapshots/snapshots.module';
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
-			type: process.env.TYPEORM_CONNECTION as any || 'mongodb',
+			type: process.env.TYPEORM_CONNECTION as any || 'mariadb',
 			host: process.env.TYPEORM_HOST || 'localhost',
-			username: process.env.TYPEORM_USERNAME || 'whois',
-			password: process.env.TYPEORM_PASSWORD || 'siohw',
+			username: process.env.TYPEORM_USERNAME || 'kootoor',
+			password: process.env.TYPEORM_PASSWORD || 'funkybudha',
 			database: process.env.TYPEORM_DATABASE || 'whois',
-			port: Number.parseInt(process.env.TYPEORM_PORT, 10) || 3306,
+			port: Number.parseInt(process.env.TYPEORM_PORT || '3306', 10),
 			entities: [Domain, NameServer, Registrar, Snapshot],
-			migrations: ['../migrations/{.js,.ts}'],
-			synchronize: true,
-			logging: true
+			migrations: ['../migrations/*.ts'],
+			synchronize: false,
+			logging: process.env.NODE_ENV !== 'production'
 		}),
 		NameServersModule,
 		RegistrarsModule,
