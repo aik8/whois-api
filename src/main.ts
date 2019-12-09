735767d7c2494c2474a65cbf9b243f8bb24c18f3
John Kitsantas <proteus@devoid.gr>
2019-12-10 00:30:57 +0200

    refactor(app): enable CORS & show current version in Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
+// Get the current package version.
+const version = require('../package.json').version;
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	// Enable CORS.
+	app.enableCors();
 
 	// Use Helmet with default settings (for starters).
 	app.use(helmet());
@@ -13,7 +19,7 @@ async function bootstrap() {
 	const options = new DocumentBuilder()
 		.setTitle('KOW Whois API')
 		.setDescription('The API that powers the KOW!')
-		.setVersion('v1.0.0')
+		.setVersion(`v${version}`)
 		.build();
 	const document = SwaggerModule.createDocument(app, options);
 	SwaggerModule.setup('swagger', app, document);
