0a41c4b80a9e5b5903fd3801685fdec322f787d6
John Kitsantas <proteus@devoid.gr>
2019-12-17 23:00:48 +0200

    chore: start a new .NET Core "Web API" project

---
+++ Models/Registrar.cs
@@ -0,0 +1,9 @@
+namespace kow_whois_api {
+	public class Registrar {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string url { get; set; }
+		public string email { get; set; }
+		public string phone { get; set; }
+	}
+}
