b7bed64a8b3cdd433d80be8f0082a59c532a7920
John Kitsantas <proteus@devoid.gr>
2019-12-23 23:22:22 +0200

    feat(PiosService): re-create the service in C#

--- Interfaces/IPiosResult.cs
+++ Interfaces/IPiosResult.cs
@@ -3,9 +3,9 @@ using KowWhoisApi.Models;
 
 namespace KowWhoisApi {
 	public interface IPiosResult {
-		Domain Domain { get; set; }
-		Registrar Registrar { get; set; }
-		ICollection<NameServer> NameServers { get; set; }
-		bool IsRegistered { get; set; }
+		Domain Domain { get; }
+		Registrar Registrar { get; }
+		ICollection<NameServer> NameServers { get; }
+		bool IsRegistered { get; }
 	}
 }
