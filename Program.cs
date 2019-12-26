c408fe77d5f8ddc5249fc2ec992f832a77525d4a
John Kitsantas <proteus@devoid.gr>
2019-12-26 13:14:28 +0200

    feat: run migrations when the services starts

--- Program.cs
+++ Program.cs
@@ -3,30 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using KowWhoisApi.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace KowWhoisApi
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var webHost = CreateHostBuilder(args).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+			using (var scope = webHost.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				try
+				{
+					var db = services.GetRequiredService<WhoisContext>();
+					db.Database.Migrate();
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occured while migrating the datbase.");
+				}
+			}
+
+			webHost.Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
 					// Make sure it listens to 127.0.0.1.
 					// (Docker does not like IPv6 that much.)
 					webBuilder.ConfigureKestrel(serverOptions =>
 						serverOptions.Listen(IPAddress.Any, 5000));
 
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
