7137c70bb9ada240e6e6641c9d42fdc51f68912b
John Kitsantas <proteus@devoid.gr>
2020-01-20 11:08:44 +0200

    refactor: break-out db-related methods to their own services

---
+++ Services/SnashotsService.cs
@@ -0,0 +1,57 @@
+using KowWhoisApi.Data;
+using KowWhoisApi.Interfaces;
+using KowWhoisApi.Models;
+
+namespace KowWhoisApi.Services
+{
+	public class SnapshotsService : ISnapshotsService
+	{
+		private WhoisContext _context;
+		private INameServersService _nameServers;
+		private IDomainsService _domains;
+		private IRegistrarsService _registrars;
+
+		public SnapshotsService(WhoisContext context, INameServersService nameServers, IDomainsService domains, IRegistrarsService registrars)
+		{
+			_context = context;
+			_nameServers = nameServers;
+			_domains = domains;
+			_registrars = registrars;
+		}
+
+		public Snapshot Create(IPiosResult piosResult)
+		{
+			var snapshot = new Snapshot();
+
+			Domain d = _domains.FindOrAdd(piosResult.Domain);
+			snapshot.Domain = d != null ? d : piosResult.Domain;
+
+			if (piosResult.IsRegistered){
+				Registrar r = _registrars.FindOrAdd(piosResult.Registrar);
+				snapshot.Registrar = r != null ? r : piosResult.Registrar;
+			}
+
+			foreach (var ns in piosResult.NameServers)
+			{
+				NameServer n = _nameServers.FindOrAdd(ns);
+
+				if (n != null)
+				{
+					snapshot.SnapshotNameServers.Add(new SnapshotNameServer { NameServer = n });
+				}
+				else
+				{
+					snapshot.SnapshotNameServers.Add(new SnapshotNameServer { NameServer = ns });
+				}
+			}
+
+			return snapshot;
+		}
+
+		public void Save(Snapshot snapshot)
+		{
+			_context.Snapshots.Add(snapshot);
+			_context.SaveChanges();
+		}
+	}
+}
