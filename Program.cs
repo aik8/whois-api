fde23dccea46494e8cfbd5e0532ad4e8065c9133
John Kitsantas <proteus@devoid.gr>
2020-02-16 08:50:19 +0200

    refactor: clean up unnecessary usings

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using KowWhoisApi.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
