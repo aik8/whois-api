3d0fb2ebe2e503d88f8ca42d9d08394c0f16c933
John Kitsantas <proteus@devoid.gr>
2020-06-09 23:31:23 +0300

    feat(ctrl:snapshots): add basic querying features

--- Controllers/SnapshotsController.cs
+++ Controllers/SnapshotsController.cs
@@ -22,35 +22,21 @@ namespace KowWhoisApi.Controllers
 		}
 
 		[HttpGet]
-		public List<Snapshot> GetAll([FromQuery] uint domainId = 0, [FromQuery] string domainName = null, [FromQuery] int per_page = 0, [FromQuery] int page = 0)
+		[Route("{id?}")]
+		public List<Snapshot> Get(uint? id, [FromQuery] uint? domainId, [FromQuery] string domainName = null, [FromQuery] int per_page = int.MaxValue, [FromQuery] int page = 0)
 		{
-			// Get the value of domainId.
-			uint id = domainId;
-
-			// If there is a domainName, ignore the domainId.
-			if (domainName != null)
-			{
-				id = _context.Domains.SingleOrDefault(d => d.Name == domainName)?.Id ?? 0;
-			}
-
-			// Run the main part of the query.
-			var query = _context.Snapshots
-					.Where(s => id == 0 ? true : s.DomainId == id)
-					.OrderBy(s => s.CreatedAt)
-					.Include(s => s.Domain)
-					.Include(s => s.Registrar)
-					.Include(s => s.SnapshotNameServers)
-					.ThenInclude(sns => sns.NameServer);
-
-			// Add pagination, if it is requested.
-			if (per_page > 0)
-			{
-				return query.Skip(per_page * page).Take(per_page).ToList();
-			}
-			else
-			{
-				return query.ToList();
-			}
+			return _context.Snapshots
+				.Where(s => id == null || s.Id == id)
+				.Where(s => domainId == null || s.DomainId == domainId)
+				.Where(s => domainName == null || s.Domain.Name == domainName)
+				.OrderBy(s => s.CreatedAt)
+				.Skip(page * per_page)
+				.Take(per_page)
+				.Include(s => s.Domain)
+				.Include(s => s.Registrar)
+				.Include(s => s.SnapshotNameServers)
+				.ThenInclude(sns => sns.NameServer)
+				.ToList();
 		}
 	}
 }
