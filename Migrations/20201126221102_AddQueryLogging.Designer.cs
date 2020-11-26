9e957f52682b0e97d2f448bde6cb60588c8ef75f
John Kitsantas <proteus@devoid.gr>
2020-11-27 00:13:13 +0200

    chore: add migrations for the new query logging stuff

--- Migrations/20201126221102_AddQueryLogging.Designer.cs
+++ Migrations/20201126221102_AddQueryLogging.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using KowWhoisApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KowWhoisApi.Migrations
 {
     [DbContext(typeof(WhoisContext))]
-    partial class WhoisContextModelSnapshot : ModelSnapshot
+    [Migration("20201126221102_AddQueryLogging")]
+    partial class AddQueryLogging
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
