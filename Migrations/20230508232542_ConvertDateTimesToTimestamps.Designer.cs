3f1f16d9b8264dfef575188cf672893985de6bef
John Kitsantas <proteus@devoid.gr>
2023-05-09 02:33:04 +0300

    chore(migrations): create migration for Timestamp conversion

--- Migrations/20230508232542_ConvertDateTimesToTimestamps.Designer.cs
+++ Migrations/20230508232542_ConvertDateTimesToTimestamps.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KowWhoisApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KowWhoisApi.Migrations
 {
     [DbContext(typeof(WhoisContext))]
-    partial class WhoisContextModelSnapshot : ModelSnapshot
+    [Migration("20230508232542_ConvertDateTimesToTimestamps")]
+    partial class ConvertDateTimesToTimestamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
