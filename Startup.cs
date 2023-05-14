2c4234d0c7d2a888d5629e470895eb9eccca50a2
John Kitsantas <proteus@devoid.gr>
2023-05-14 16:56:23 +0300

    feat(startup): register the Addresses service

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,7 @@ namespace KowWhoisApi
 			services.AddTransient<IRegistrarsService, RegistrarsService>();
 			services.AddTransient<INameServersService, NameServersService>();
 			services.AddTransient<ISnapshotsService, SnapshotsService>();
+			services.AddTransient<IAddressesService, AddressesService>();
 
 			services.AddCors(options =>
 			{
